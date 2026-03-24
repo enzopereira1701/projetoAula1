60615848233a6daaae017753d1541c81dd093ca7
Enzo <enzopereira1701@gmail.com>
2026-03-24 11:44:28 -0300

    feet=declarações e variáveis JS

---
+++ src/js/script.js
@@ -0,0 +1,50 @@
+// Declarações e variáveis 
+// var, let e const 
+var x=10; 
+console.log(x);
+var x=20;
+console.log(x)
+
+// var pode redeclarar e acessar fora do escopo
+// let não pode redeclarar e não consegue acessar fora do escopo
+
+if(true){
+    let nome="Fiap"
+    console.log(nome)
+}
+
+//const
+//{
+  //  const taxaJuros = 0.85;
+   // taxaJuros = 0.50;
+   // console.log(taxaJuros);
+//}
+
+var exemplo1="Ola dev";
+console.log(exemplo1);
+
+let exemplo2="Ola dev let";
+console.log(exemplo2);
+
+const exemplo3="Ola dev const";
+console.log(typeof exemplo3);
+
+// numero sem nada texto com """"
+let exemplo4=20;
+console.log(typeof exemplo4)
+
+let exemplo5=true;
+console.log(typeof exemplo5);
+
+let exemplo6={nome:"Fiap"};
+console.log(typeof exemplo6)
+
+let exemplo7=["Java", "Python"," C"]
+console.log(exemplo7)
+
+let exemplo8; //indefinido
+console.log(exemplo8)
+
+let exemplo9=null;
+console.log(exemplo9) // variavel vazia
+//() = metodo {} = objeto [] = array 
