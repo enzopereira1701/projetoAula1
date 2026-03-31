834a446fc4d61393e16e972507423d6ccb281115
enzo <enzopereira1701@gmail.com>
2026-03-31 10:37:23 -0300

    feat: Operadores aritméticos, lógicos e comparação

--- src/js/script.js
+++ src/js/script.js
@@ -1,50 +1,79 @@
-// Declarações e variáveis 
-// var, let e const 
-var x=10; 
-console.log(x);
-var x=20;
-console.log(x)
+// // Declarações e variáveis 
+// // var, let e const 
+// var x=10; 
+// console.log(x);
+// var x=20;
+// console.log(x)
 
-// var pode redeclarar e acessar fora do escopo
-// let não pode redeclarar e não consegue acessar fora do escopo
+// // var pode redeclarar e acessar fora do escopo
+// // let não pode redeclarar e não consegue acessar fora do escopo
 
-if(true){
-    let nome="Fiap"
-    console.log(nome)
-}
+// if(true){
+//     let nome="Fiap"
+//     console.log(nome)
+// }
 
-//const
-//{
-  //  const taxaJuros = 0.85;
-   // taxaJuros = 0.50;
-   // console.log(taxaJuros);
-//}
+// //const
+// //{
+//   //  const taxaJuros = 0.85;
+//    // taxaJuros = 0.50;
+//    // console.log(taxaJuros);
+// //}
 
-var exemplo1="Ola dev";
-console.log(exemplo1);
+// var exemplo1="Ola dev";
+// console.log(exemplo1);
 
-let exemplo2="Ola dev let";
-console.log(exemplo2);
+// let exemplo2="Ola dev let";
+// console.log(exemplo2);
 
-const exemplo3="Ola dev const";
-console.log(typeof exemplo3);
+// const exemplo3="Ola dev const";
+// console.log(typeof exemplo3);
 
-// numero sem nada texto com """"
-let exemplo4=20;
-console.log(typeof exemplo4)
+// // numero sem nada texto com """"
+// let exemplo4=20;
+// console.log(typeof exemplo4)
 
-let exemplo5=true;
-console.log(typeof exemplo5);
+// let exemplo5=true;
+// console.log(typeof exemplo5);
 
-let exemplo6={nome:"Fiap"};
-console.log(typeof exemplo6)
+// let exemplo6={nome:"Fiap"};
+// console.log(typeof exemplo6)
 
-let exemplo7=["Java", "Python"," C"]
-console.log(exemplo7)
+// let exemplo7=["Java", "Python"," C"]
+// console.log(exemplo7)
 
-let exemplo8; //indefinido
-console.log(exemplo8)
+// let exemplo8; //indefinido
+// console.log(exemplo8)
 
-let exemplo9=null;
-console.log(exemplo9) // variavel vazia
-//() = metodo {} = objeto [] = array 
+// let exemplo9=null;
+// console.log(exemplo9) // variavel vazia
+// //() = metodo {} = objeto [] = array 
+
+// Operadores aritimeticos 
+const valor1=20;
+const valor2=10;
+
+console.log(valor1 + valor2 );
+console.log(valor1 / valor2 );
+console.log(valor1 - valor2 );
+console.log(valor1 * valor2 );
+
+// operadores lógicos 
+// < menor > maior = atribuir == comparar ==- =! diferente
+//  && e || ou 
+
+console.log(valor1<valor2);
+console.log(valor1>valor2);
+console.log(valor1 > 10 && valor2 != 20);
+console.log(valor2 < 5 || valor1 > 50);
+
+// operadores de comparação 
+console.log(valor1 < valor2);
+console.log(valor1 <= valor2);
+console.log(valor1 > valor2);
+console.log(valor1 >= valor2);
+console.log(valor1 != valor2);
+console.log(valor1 == valor2); // compara
+console.log(valor1 === valor2); // compara e olha o tipo da variavel 
+console.log("O valor é", valor2);
+
